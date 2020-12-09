ee124f311543b15f3513edb0858860ae24e36cc6
Alexis Prétot <74535765+Pretalex@users.noreply.github.com>
2020-12-09 15:33:20 +0100

    whoIs Update

--- whoIs.php
+++ whoIs.php
@@ -1,1 +1,15 @@
-<p>je suis la partie services</p>
+<div class="whois container-fluid">
+    <div class="container">
+    <h2 class="whois_titre">Who is john doe ?</h2>
+        <div class="row">
+            <div class="whois_desc col-6">
+                <p>Bear claw marzipan bear claw applicake i love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps i love underwear.com. Soufflé cotton candy dessert candyice cream wafer gummies cheesecake brownie.</p>
+                <p>Bear claw marzipan bear claw applicake i love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps i love underwear.com. Soufflé cotton candy dessert candyice cream wafer gummies cheesecake brownie.</p>
+            </div>
+            <div class="whois_desc col-6"> 
+                <p>Bear claw marzipan bear claw applicake i love muffin. Lemon drops gummi bears pastry gummi bears sesame snaps i love underwear.com. Soufflé cotton candy dessert candyice cream wafer gummies cheesecake brownie.</p>
+                <p class="whois_icon"><a href="#"><img src="images/social/twiter2.png" alt=""></a><a href="#"><img src="images/social/googleplus.png" alt=""></a><a href="#"><img src="images/social/dribble.png" alt=""></a><a href="#"><img src="images/social/coffee2.png" alt=""></a></p>
+            </div>
+        </div>
+    </div>
+</div>
