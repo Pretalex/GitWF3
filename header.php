8a05e329f3348422b4a0e2662d3580e8d69f3656
Souheil20 <74536739+Souheil20@users.noreply.github.com>
2020-12-09 14:06:37 +0100

    Add files via upload

--- header.php
+++ header.php
@@ -1,1 +1,18 @@
-<p>je suis le header</p>
+ <header>
+         <nav>
+            <p class="p_logo"> BLU<span class="span_E">E</span>ASY</p>
+              <ul class="ul_menu">
+                <li class="li_item"><a class="home" href="#">Home</a></li>
+                <li class="li_item"><a href="#">Services</a></li>
+                <li class="li_item"><a href="#">Features</a></li>
+                <li class="li_item"><a href="#">Portfolio</a></li>
+                <li class="li_item"><a href="#">Contact</a></li>
+              </ul>
+           </nav>
+
+      <div class="div_texte">
+        <p class="p1"> "I am looking for the unexpected.</p>
+
+        <p class="p2">i am looking for things i have never seen before"</p>
+      </div>
+</header>
