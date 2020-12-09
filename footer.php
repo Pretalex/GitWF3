e9906f24d883fc77672db7a0ed7a4a505a8b92f5
Souheil20 <74536739+Souheil20@users.noreply.github.com>
2020-12-09 15:27:59 +0100

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,1 +1,10 @@
-<p>je suis la partie services</p>
+<footer>
+<ul class="ul_footer">
+    <li class="il_footer">Home</li>
+    <li class="il_footer">Services</li>
+    <li class="il_footer">Portfolio</li>
+    <li class="il_footer">Contact</li>
+</ul>
+
+<span class="année">2020©</span>
+</footer>
