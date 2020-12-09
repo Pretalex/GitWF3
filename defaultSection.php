a0e63573b714c3a200b7556f05849bd0f49615ad
Alexis Prétot <74535765+Pretalex@users.noreply.github.com>
2020-12-09 15:07:06 +0100

    Default Section Update

--- defaultSection.php
+++ defaultSection.php
@@ -1,1 +1,14 @@
-<p>je suis la partie services</p>
+<div class='container-fluid defaultSection_background'>
+    <div class='container defaultSection_contenu'>
+        <div class='defaultSection_partie defaultSection_partie1'>
+            <h2>JUST DEFAULT SECTION</h2>
+            <hr>
+            <p class='defaultSection_bold' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae cum odio tempore ut id nostrum natus, accusamus deleniti magnam numquam.</p>
+            <p class='defaultSection_bold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae cum odio tempore ut id nostrum natus, accusamus deleniti magnam numquam.</p>
+            <button class='defaultSection_button'>VISIT ME</button>
+        </div>
+        <div class='defaultSection_partie defaultSection_partie2'>
+        <iframe class='defaultSection_video' src="https://www.youtube.com/embed/hwMMImUvkGs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture " allowfullscreen></iframe>
+        </div>
+    </div>
+</div>
