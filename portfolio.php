326218d7e3eb6f4cd9f80bc54b9d8c6cc3cbdcfe
CaroleDEMIT <74536224+CaroleDEMIT@users.noreply.github.com>
2020-12-09 15:32:21 +0100

    Add files via upload

--- portfolio.php
+++ portfolio.php
@@ -1,1 +1,74 @@
-<p>je suis la partie portfolio</p>
+
+
+<div class="container-fluid">
+
+   <div class="container">
+
+        <div class="portfolio-header">
+               
+                    <h3>TFOLIO</h3>
+                    <ul>
+                       <a  class="nav-link" href="#">ALL</a>
+                       <a  class="nav-link" href="#">PANORAMA</a>
+                       <a  class="nav-link" href="#">PORTAITS</a>
+                       <a  class="nav-link" href="#">MACROS</a>
+                       <a  class="nav-link" href="#">JOURNAL</a>
+                    </ul>
+                
+        </div>  
+
+
+
+        <div class="portfolio-photos">
+            
+               <div class="row row-cols-1 row-cols-md-2 g-4">
+                    <div class="col">                        
+                      <a href=""><img src="images/portfolio/1.png" class="card-img" alt=""/></a>                       
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                        <a href=""><img src="images/portfolio/2.png" class="card-img" alt=""/></a>      
+                        </div>
+                    </div>
+                    <div class="col">
+                         <div class="card">
+                         <a href=""><img src="images/portfolio/3.png" class="card-img" alt=""/></a>    
+                         </div>
+                    </div>
+                    <div class="col">
+                         <div class="card">
+                         <a href=""><img src="images/portfolio/4.png" class="card-img" alt=""/></a>    
+                         </div>
+                    </div>    
+                </div>
+
+        <br>
+
+               <div class="row row-cols-1 row-cols-md-2 g-4">
+                    <div class="col">
+                        <div class="card">
+                        <a href=""><img src="images/portfolio/5.png" class="card-img" alt=""/></a>     
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                        <a href=""><img src="images/portfolio/6.png" class="card-img" alt=""/></a>    
+                        </div>
+                    </div>
+                    <div class="col">
+                         <div class="card">
+                         <a href=""><img src="images/portfolio/7.png" class="card-img" alt=""/></a>     
+                         </div>
+                    </div>
+                    <div class="col">
+                         <div class="card">
+                         <a href=""><img src="images/portfolio/8.png" class="card-img" alt=""/></a>    
+                         </div>
+                    </div>
+                </div>
+        </div>   
+
+           
+   </div>
+
+</div>
