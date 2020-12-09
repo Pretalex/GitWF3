6836b2593b20904562cf4856c7ce3f7432a39a14
Alexis Prétot <74535765+Pretalex@users.noreply.github.com>
2020-12-09 13:36:20 +0100

    Alexis Partie 1

---
+++ twitter.php
@@ -0,0 +1,10 @@
+<div class='container-fluid twitter_background'>
+    <div class='container'>
+        <div class='twitter_spacebetween'>
+            <h2>Twitter</h2><i class="fab fa-twitter twitter_icon"></i>
+        </div>
+        <hr class='twitter_hr'>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae cum odio tempore ut id nostrum natus, accusamus deleniti magnam numquam.</p>
+        <p><span class='twitter_bold'>@Lorem ipsum dolor</span> sit amet consectetur adipisicing elit.</p>
+    </div>
+</div>
