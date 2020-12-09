21b7b5034a554d534e3b3931ff66e3a6beb62462
Thony <75620714+anthonyrubin1@users.noreply.github.com>
2020-12-09 14:49:56 +0100

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,1 +1,43 @@
-<p>je suis la partie services</p>
+<div class="container-fluid" id="container-formulaire">
+
+    <section-formulaire>
+
+    
+    
+    <form>
+
+    <h2 class="titre-contact"><span class="spanser">CON</span>TACT</h2>
+
+    
+        <div class="form-group" id="namemail-contact">
+            <label for="exampleFormControlInput1">FULL NAME</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1">
+        </div>
+
+        <div class="form-group" id="namemail-contact">
+            <label for="exampleFormControlInput1">EMAIL</label>
+            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+  
+  
+        <div class="form-group">
+            <label for="exampleFormControlTextarea1">MESSAGE</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+        </div>
+
+        <button type="submit" class="btn-contact">MESSAGE</button>
+
+        <p class="paracontact">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae placeat itaque deleniti culpa obcaecati neque nobis soluta, sint asperiores. Illum repellat, fuga eligendi ab quidem quisquam ratione culpa distinctio quae.</p>
+
+    </form>
+      
+
+    </section-formulaire>
+
+
+
+</div>
+
+
+
+</div>
--- services.php
+++ services.php
@@ -1,1 +1,42 @@
-<p>je suis la partie services</p>
+<div class="container-fluid" id="container-services">
+
+    <section-services>
+
+    <h2 class="titre-services"><span class="spanser">SER</span>VICES</h2>
+    
+    
+        <div class="elements-services">
+            <div class="elem-services">
+                <img src="images/coffee.png" alt="img">
+                <h3 class="titrelem-services">COFFEE</h3>
+                <p class="para-services">Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate modi in debitis ab excepturi aliquid aspernatur accusamus, nisi magni consectetur vero ducimus architecto, ut inventore facere nemo laboriosam error nam?</p>
+            </div>
+
+            <div class="elem-services">
+                <img src="images/instant.png" alt="img">
+                <h3 class="titrelem-services">INSTANT</h3>
+                <p class="para-services">Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate modi in debitis ab excepturi aliquid aspernatur accusamus, nisi magni consectetur vero ducimus architecto, ut inventore facere nemo laboriosam error nam?</p>
+            </div>
+
+            <div class="elem-services">
+                 <img src="images/frame.png" alt="img">
+                <h3 class="titrelem-services">SERIOUS</h3>
+                <p class="para-services">Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate modi in debitis ab excepturi aliquid aspernatur accusamus, nisi magni consectetur vero ducimus architecto, ut inventore facere nemo laboriosam error nam?</p>
+            </div>
+
+            <div class="elem-services">
+                <img src="images/frame.png" alt="img">
+                <h3 class="titrelem-services">FRAME</h3>
+                <p class="para-services">Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate modi in debitis ab excepturi aliquid aspernatur accusamus, nisi magni consectetur vero ducimus architecto, ut inventore facere nemo laboriosam error nam?</p>
+            </div>
+
+    </section-services>
+
+
+
+</div>
+
+
+
+</div>
+
