e901e7bf522942329329e0f9418ba9706bc5b6a3
Thony <75620714+anthonyrubin1@users.noreply.github.com>
2020-12-09 15:17:57 +0100

    Add files via upload

--- testimonials.php
+++ testimonials.php
@@ -1,1 +1,21 @@
-<p>je suis la partie services</p>
+<div class="container-fluid" id="container-testimonials">
+
+    <section-testimonials>
+
+    
+
+    <h2 class="titre-testimonials"><span class="spanser">TES</span>TIMONIALS <img class="imgui" src="images/guillemets.png" alt=""></h2>
+
+        <p class="paratesti">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis magni amet delectus eveniet ipsa enim qui itaque impedit, nobis hic. Recusandae, earum sunt. Minima eaque suscipit cupiditate. Dolorum, temporibus eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam eaque odit eveniet soluta omnis ipsam ad dolores dolorum non itaque, ab amet quasi earum quam nesciunt qui maiores? Earum, minus.</p>
+
+        <p class="walter">-WALTER WHITE</p>
+
+    </section-testimonials>
+
+
+
+</div>
+
+
+
+</div>
